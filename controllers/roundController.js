c79de8312114d87af86ac2ee4a60a0bb89ba7ede
Mitch Bilensky <mitchbilensky@gmail.com>
2018-06-14 19:59:58 -0300

    Fixed saving rounds and added URL courseID param
    courseId parameter selected course to enter date for, selected
    courseId=1 by default, to be updated later

--- controllers/roundController.js
+++ controllers/roundController.js
@@ -1,36 +1,65 @@
 rounds.controller("roundController", function($scope, $http, $location) {
 
+    var courseId = $location.search().courseId ? $location.search().courseId : 1;
+    var course = {};
+
     $scope.round = {};
     $scope.round.playerRounds = [];
     $http.defaults.useXDomain = true;
-
-    var courseId = $location.search().courseId;
-
-    if (!courseId){
-      courseId = 1;
-    }
+    $scope.round.courseId = courseId;
 
     $http.get(window.__env.apiUrl + window.__env.baseUrl + "courses/" + courseId).then(function(response){
       $scope.course = response.data;
+      course = $scope.course;
     });
     $http.get(window.__env.apiUrl + window.__env.baseUrl + "players").then(function(response){
       $scope.players = response.data;
     });
 
+    $scope.course = course;
+
     $scope.addPlayer = function(){
-      var playerRound = {};
-      playerRound.player = $scope.newPlayer;
-      $scope.round.playerRounds.push(playerRound);
-      var index = $scope.players.map(function(e) { return e.id; }).indexOf($scope.newPlayer.id);
+      if($scope.newPlayer){
+        var playerRound = {};
+        playerRound.scores = [];
+        for (var i = 0; i < course.holes.length; i++){
+          playerRound.scores[i] = {};
+          playerRound.scores[i].hole = course.holes[i]
+        }
+        playerRound.player = $scope.newPlayer;
+        $scope.round.playerRounds.push(playerRound);
+        var index = $scope.players.map(function(e) { return e.id; }).indexOf($scope.newPlayer.id);
+        if (index != -1){
+          $scope.players.splice(index, 1);
+        };
+      }
+    }
+
+    $scope.removePlayer = function(playerRound){
+      var index = $scope.round.playerRounds.map(function(e) { return e.player.id; }).indexOf(playerRound.player.id);
       if (index != -1){
-        $scope.players.splice(index, 1);
+        $scope.round.playerRounds.splice(index, 1);
       };
+      $scope.players.push(playerRound.player);
     }
 
     $scope.getTotal = function(playerRound){
       playerRound.totalStrokes = 0;
-      for(var score in playerRound.scores){
-        playerRound.totalStrokes += parseInt(playerRound.scores[score].strokes);
+      for (var i = 0; i < playerRound.scores.length; i++){
+        playerRound.totalStrokes += parseInt(playerRound.scores[i].strokes);
       }
+    }
+
+    $scope.saveRound = function(round){
+      $http.post(window.__env.apiUrl + window.__env.baseUrl + "rounds", $scope.round)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(response) {
+          console.log(response);
+        })
+        .finally(function() {
+          console.log("Finally!");
+        });
     }
 });
